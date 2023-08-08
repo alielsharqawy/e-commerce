21b6b09814f1d7a2579923dffb49d3f2f2e687aa
ahmedhossam1212 <pmedo1689@gmail.com>
2023-08-09 00:23:21 +0300

    edit login & register them

--- lib/widget/form_field.dart
+++ lib/widget/form_field.dart
@@ -1,5 +1,8 @@
+import 'package:app/data_cubit/cubit/user_cubit/user_cubit.dart';
+import 'package:app/data_cubit/states/user_states/user_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Widget defaultFormField({
   TextEditingController? controller,
@@ -17,27 +20,37 @@ Widget defaultFormField({
   List<TextInputFormatter>? textInputFormatter,
   TextInputType? keyboardType,
 }) =>
-    Container(
-      width: width,
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: TextFormField(
-        controller: controller,
-        keyboardType: keyboardType,
-        obscureText: isPassword,
-        enabled: isClickable,
-        onTap: () {},
-        validator: (String? s) {
-          return validate(s);
-        },
-        onChanged: onChange,
-        inputFormatters: textInputFormatter,
-        decoration: InputDecoration(
-          label: Text(hint),
-          hintStyle:
-              Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 15.0),
-        ),
-      ),
+    BlocConsumer<UserCubit, UserState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        var cubit = UserCubit.get(context);
+        return Container(
+          width: width,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: TextFormField(
+            controller: controller,
+            keyboardType: keyboardType,
+            obscureText: isPassword,
+            enabled: isClickable,
+            onTap: () {},
+            validator: (String? s) {
+              return validate(s);
+            },
+            onChanged: onChange,
+            inputFormatters: textInputFormatter,
+            decoration: InputDecoration(
+              fillColor: cubit.isdark ? Colors.white : Colors.white,
+              filled: true,
+              label: Text(hint),
+              hintStyle: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(fontSize: 15.0),
+            ),
+          ),
+        );
+      },
     );
